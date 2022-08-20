705b487d396971ba2cd31231c83488135f7534c1
Sherilyn Maestre <you@example.com>
2022-08-20 15:52:06 -0500

    feature: general container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
+import Container from './components/container';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
    <>
    <Navbar/>
+   <Container>
+    <p>Hola</p>
+   </Container>
    </>
   );
 }
