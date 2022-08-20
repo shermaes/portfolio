03894d6990a302ca61d0d61403e83c7964c3003d
Sherilyn Maestre <you@example.com>
2022-08-20 14:39:40 -0500

    feature: navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import * as S from "./Navbar.styles";
-
+import  LogoSher from '../../img/logo_sher.jpeg'
 const Navbar = () => {
   return (
-    <S.ContainerNavbar> Nuestro container</S.ContainerNavbar>
+    <S.ContainerNavbar> 
+      <S.ContainerLogo>
+        <img src={LogoSher} alt="Sher Maestre" />
+        <div>
+          <h1>Sherilyn Maestre</h1>
+          <h2>Frontend Developer</h2>
+        </div>
+      </S.ContainerLogo>
+    </S.ContainerNavbar>
   )
 }
 
--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -11,3 +11,29 @@ max-width: 100vw;
   padding: 0 20px;
   background: ${colors.TEXT_WHITE};
 `;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  align-items: center;
+  h1,
+  h2 {
+    font-weight: 700;
+    color: ${colors.TEXT_BLACK};
+    margin: 0;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+    background: linear-gradient(110deg, #B22222 0%, #FFA07A 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  img{
+    width: 130px;
+    padding-right: 20px;
+  }
+`;
+
+
