9f51340afe5387d836c70cb4e043c5fd1b264a8f
Sherilyn Maestre <you@example.com>
2022-08-20 12:14:41 -0500

    feature: global colors

--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
+import { colors } from "../../styles/colors";
 
-export const ContainerNavbar = styled.div`
-height: 100px;
-background-color: aliceblue;
-`;  
+export const ContainerNavbar =styled.div`
+max-width: 100vw;
+  height: 100px;
+  box-shadow: ${colors.BOX_SHADOW};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  background: ${colors.TEXT_WHITE};
+`;
